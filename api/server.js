c810acf5fddce80783cf9a147e333c5b371e5c0c
krunal Patel <pkrunal616@gmail.com>
2019-12-17 14:14:46 -0500

    today guided work

--- api/server.js
+++ api/server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const sessions = require('express-session');
+const knexSessionConnect = require('connect-session-knex')(sessions)
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const databass = require('../database/dbConfig.js');
 
 const server = express();
+
+sessionCofiguration = {
+  name: 'cookieemonster',
+  secret: 'keep it secret',
+  saveUninitialized: true,
+
+  store: new knexSessionConnect({
+    databass,
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 18,
+  }),
+
+  cookie: {
+    maxAge: 1000 * 60 * 18,
+    secure: false,
+    httpOnly: true
+  }
+}
+
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(sessions(sessionCofiguration))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
